1f66c14b2decf0903b5d7722e72d160e821a884c
jvanbrie <jon.vanbriesen@datadoghq.com>
2022-03-05 14:47:44 -0500

    trying to fix client

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { View, Switch, StyleSheet } from "react-native";
 import TcpSocket from 'react-native-tcp-socket';
+import Server from "react-native-tcp-socket/lib/types/Server";
+import Socket from "react-native-tcp-socket/lib/types/Socket";
 
 const App = () => {
   const [isEnabled, setIsEnabled] = useState(false);
@@ -13,6 +15,9 @@ const App = () => {
     }
   }
 
+  var client = useRef<null | Socket>(null); 
+  var server = useRef<null | Server>(null); 
+
   const connectionOptions = {
     port: 58888,
     host: '10.0.0.111',
@@ -20,29 +25,31 @@ const App = () => {
   }
 
   const createClient = () => {
-    const client = TcpSocket.createConnection(connectionOptions, () => {
+    client.current = TcpSocket.createConnection(connectionOptions, () => {
       // Write on the socket
-      client.write('Hello server!');
-    
-      // Close socket
-      client.destroy();
+      if (client.current !== null){
+        client.current.write('Hello server!');
+      
+        // Close socket
+        client.current.destroy();
+      }
     });
     
-    client.on('data', function(data) {
+    client.current.on('data', function(data) {
       console.log('message was received', data);
     });
     
-    client.on('error', function(error) {
+    client.current.on('error', function(error) {
       console.log(error);
     });
     
-    client.on('close', function(){
+    client.current.on('close', function(){
       console.log('Connection closed!');
     });  
   }
 
   const createServer = () => {
-    const server = TcpSocket.createServer(function(socket) {
+    server.current = TcpSocket.createServer(function(socket) {
       socket.on('data', (data) => {
         socket.write('Echo server ' + data);
       });
@@ -55,14 +62,15 @@ const App = () => {
         console.log('Closed connection with ', socket.address());
       });
     }).listen(connectionOptions);
-    
-    server.on('error', (error) => {
-      console.log('An error ocurred with the server', error);
-    });
-    
-    server.on('close', () => {
-      console.log('Server closed connection');
-    });    
+      if (server.current !== null){  
+      server.current.on('error', (error) => {
+        console.log('An error ocurred with the server', error);
+      });
+      
+      server.current.on('close', () => {
+        console.log('Server closed connection');
+      });    
+    }
   }
 
   return (
