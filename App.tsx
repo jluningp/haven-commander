4c43c65d430e0689c00ecf9e6c2cbe2a1124f70a
jvanbrie <jon.vanbriesen@datadoghq.com>
2022-03-05 14:19:01 -0500

    adding support for server/client

--- App.tsx
+++ App.tsx
@@ -1,11 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Switch, StyleSheet } from "react-native";
+import TcpSocket from 'react-native-tcp-socket';
 
-export default function App() {
+const App = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => {
+    setIsEnabled(previousState => !previousState);
+    if (isEnabled){
+      createServer()
+    } else {
+      createClient()
+    }
+  }
+
+  const connectionOptions = {
+    port: 58888,
+    host: '10.0.0.111',
+    tls: true,
+  }
+
+  const createClient = () => {
+    const client = TcpSocket.createConnection(connectionOptions, () => {
+      // Write on the socket
+      client.write('Hello server!');
+    
+      // Close socket
+      client.destroy();
+    });
+    
+    client.on('data', function(data) {
+      console.log('message was received', data);
+    });
+    
+    client.on('error', function(error) {
+      console.log(error);
+    });
+    
+    client.on('close', function(){
+      console.log('Connection closed!');
+    });  
+  }
+
+  const createServer = () => {
+    const server = TcpSocket.createServer(function(socket) {
+      socket.on('data', (data) => {
+        socket.write('Echo server ' + data);
+      });
+    
+      socket.on('error', (error) => {
+        console.log('An error ocurred with client socket ', error);
+      });
+    
+      socket.on('close', (error) => {
+        console.log('Closed connection with ', socket.address());
+      });
+    }).listen(connectionOptions);
+    
+    server.on('error', (error) => {
+      console.log('An error ocurred with the server', error);
+    });
+    
+    server.on('close', () => {
+      console.log('Server closed connection');
+    });    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
   );
 }
@@ -13,8 +81,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+export default App;
